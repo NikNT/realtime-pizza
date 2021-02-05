eefc061f402a13a386094292eb062828d097cf9d
Nikhil Tanwar <nikhiltanwarnt@gmail.com>
2021-02-06 00:50:30 +0530

    Version Initial - Realtime Pizza

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express'); 
+const app = express(); 
+
+const PORT = process.env.PORT || 3000; 
+
+const ejs = require('ejs');
+const expressLayout = require('express-ejs-layouts');
+const path = require('path'); 
+
+app.listen(PORT, () => {
+
+    console.log(`Server is Now Active on Port Number: ${PORT}`);
+
+});
+
+app.get('/',(req,res)=>{
+
+    res.render('home'); 
+
+})
+
+app.use(expressLayout); 
+app.set('views',path.join(__dirname,'resources/views'));
+app.set('view engine','ejs'); 
