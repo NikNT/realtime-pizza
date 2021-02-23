3232db72b83d4397ed1bcfb408e1540a93620bca
Nikhil Tanwar <nikhiltanwarnt@gmail.com>
2021-02-23 17:12:58 +0530

    Add to Cart Functionality

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,53 @@
-console.log('Hello from app.js (resources folder)')
+import axios from 'axios'
+import Noty from 'noty'
+
+let addToCart = document.querySelectorAll('.add-to-cart')
+
+let cartCounter = document.querySelector('#cartCounter')
+
+function updateCart(pizza){
+    //Send Request to Server
+
+    axios.post('/update-cart', pizza).then(res =>{
+
+        cartCounter.innerText = res.data.totalQty
+
+        new Noty ({
+
+            type: 'success', 
+            
+            timeout: 2000, 
+
+            text: 'Item Added to Cart'
+
+        }).show(); 
+
+    }).catch(err => {
+
+        new Noty ({
+
+            type: 'error', 
+            
+            timeout: 2000, 
+
+            text: 'Oops! Something Went Wrong! '
+
+        }).show(); 
+
+
+
+
+
+    })
+
+}
+
+
+addToCart.forEach((btn) => {
+    btn.addEventListener('click', (e) =>{
+        let pizza = JSON.parse(btn.dataset.pizza)
+        updateCart(pizza)
+
+
+    })
+})
--- server.js
+++ server.js
@@ -1,7 +1,56 @@
+require('dotenv').config()
+
 const express = require('express'); 
 const app = express(); 
 
 const PORT = process.env.PORT || 3000; 
+
+const mongoose = require('mongoose'); 
+
+const session  = require('express-session');
+
+const flash = require('express-flash'); 
+
+const MongoDbStore = require('connect-mongo')(session) 
+
+
+//Database Connection 
+
+const url = 'mongodb://localhost/pizza';
+mongoose.connect(url, { useNewUrlParser: true, useCreateIndex:true, useUnifiedTopology: true, useFindAndModify: true});
+
+const connection = mongoose.connection; 
+connection.once('open', ()=>{
+
+console.log('Database Successfully Connected!')
+
+}).catch(err =>{
+
+console.log('Connection Failed!') 
+}); 
+
+//Session Store 
+
+let mongoStore = new MongoDbStore({
+
+                mongooseConnection: connection, 
+                collection: 'sessions'
+
+            }) 
+
+//Session Config - Acts as Middleware
+
+app.use(session({
+
+    secret: process.env.COOKIE_SECRET, 
+    resave: false, 
+    store: mongoStore, 
+    saveUninitialized: false,  
+    cookie: { maxAge: 1000 * 60 * 60 * 24}
+
+}));
+
+app.use(flash()); 
 
 const ejs = require('ejs');
 const expressLayout = require('express-ejs-layouts');
@@ -14,6 +63,18 @@ app.listen(PORT, () => {
 });
 
 app.use(express.static('public')); 
+app.use(express.json()); 
+
+
+//Global Middleware 
+
+app.use((req,res,next) => {
+
+    res.locals.session = req.session
+    next ()
+
+})
+
 
 //Set Template Engine
 
@@ -21,23 +82,5 @@ app.use(expressLayout);
 app.set('views',path.join(__dirname,'resources/views'));
 app.set('view engine','ejs'); 
 
-app.get('/',(req,res)=>{
+require('./routes/web') (app)
 
-    res.render('home'); 
-
-})
-
-app.get("/cart", (req,res)=>{
-
-    res.render('customers/cart')
-})
-
-app.get("/login", (req,res)=>{
-
-    res.render('auth/login')
-})
-
-app.get("/register", (req,res)=>{
-
-    res.render('auth/register')
-})
